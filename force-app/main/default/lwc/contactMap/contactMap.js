4d8e62ed39b317c6bf751fa8f48767592d02ce31
Sudhir Dhawan <Sudhawan2@gmail.com>
2022-10-10 23:57:44 +0530

    Component addition -10/10 conditionally render

--- force-app/main/default/lwc/contactMap/contactMap.js
+++ force-app/main/default/lwc/contactMap/contactMap.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track} from 'lwc';
 
-export default class ContactMap extends LightningElement {}
+export default class ContactMap extends LightningElement {
+    @track message="";
+    @track modalVisibilty=false;
+    handleChange(event){
+        this.message=event.target.value;
+        if(this.message==="Something New Is Coming"){
+            this.modalVisibilty=true;
+        }else{
+            this.modalVisibilty=false;
+        }
+    }
+}
